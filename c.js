7e188388eb2e765254b170d8702844aaf1a7424a
wanying.wang <wanying.wang@17zuoye.com>
2018-07-18 17:02:47 +0800

    pushed commit
    pushed commit second
    
    pushed commit third

--- c.js
+++ c.js
@@ -3,5 +3,10 @@ console.log('second commit')
 console.log('third commit')
 
 
+<<<<<<< HEAD
+=======
 
 console.log('first pushed commit')
+console.log('second pushed commit')
+console.log('third pushed commit')
+>>>>>>> 9c9b3a0... pushed commit third
